05fdcc5d42d8b61806b2f7a5e3cb4f1ce0159378
shanecanny1 <36367839+shanecanny1@users.noreply.github.com>
2019-10-28 21:55:50 +0000

    Week 3 Lab Part 1
    Week 3 Lab Part 1

---
+++ week03-webScraping/week3pt1.py
@@ -0,0 +1,9 @@
+## Part 1 of Lab Week 3 ##
+## Shane Canny ##
+## 28 Oct 2019 ##
+
+import requests
+page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+print (page)
+print("-------------------")
+print (page.content)
